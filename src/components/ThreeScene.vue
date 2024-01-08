ee7dad4985ccfcbd880c5070c34120d30f252770
Golfier-Maxime <maxime.golfier@outlook.fr>
2024-01-08 15:30:44 +0100

    three js + montre dae

--- src/components/ThreeScene.vue
+++ src/components/ThreeScene.vue
@@ -4,61 +4,114 @@
 
 <script>
 import * as THREE from "three";
+import { ColladaLoader } from "three/addons/loaders/ColladaLoader.js";
+import OrbitControls from "three-orbit-controls";
 
 export default {
   data() {
-    return {
-      scene: null,
-      camera: null,
-      renderer: null,
-      cube: null,
-    };
+    return {};
   },
   mounted() {
     this.init();
-    this.animate();
   },
   methods: {
     init() {
-      // Créer une scène
-      this.scene = new THREE.Scene();
+      // Create a scene
+      const scene = new THREE.Scene();
 
-      // Créer une caméra
-      this.camera = new THREE.PerspectiveCamera(
+      // Create a camera
+      const camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       );
-      this.camera.position.z = 5;
+      camera.position.z = 100;
 
-      // Créer un renderer
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize(1000, 600);
+      // Create a renderer
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(1300, 700);
+      this.$refs.scene.appendChild(renderer.domElement);
 
-      // Ajouter le renderer au DOM
-      this.$refs.scene.appendChild(this.renderer.domElement);
+      // Load the DAE model
 
-      // Ajouter un cube à la scène
-      const geometry = new THREE.BoxGeometry(2, 2, 2); // Modifier les dimensions ici
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      this.cube = new THREE.Mesh(geometry, material);
-      this.scene.add(this.cube);
-    },
-    animate() {
-      requestAnimationFrame(this.animate);
+      const loader = new ColladaLoader();
+      loader.load("./models/montre.dae", (collada) => {
+        const daeModel = collada.scene;
 
-      // Faire des modifications ou animations ici
-      this.cube.rotation.x += 0.01;
-      this.cube.rotation.y += 0.01;
+        const boitierRond = daeModel.getObjectByName("boitier_rond");
+        boitierRond.material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        // Add the "boitier_rond" object to the scene
+        scene.add(boitierRond);
 
-      // Rendre la scène
-      this.renderer.render(this.scene, this.camera);
+        const aiguilleSecondes = daeModel.getObjectByName("aiguille_secondes");
+        aiguilleSecondes.material = new THREE.MeshBasicMaterial({
+          color: 0x00ff00,
+        });
+        // Add the "aiguille_secondes" object to the scene
+        scene.add(aiguilleSecondes);
+
+        const iBouton = daeModel.getObjectByName("bouton");
+        iBouton.material = new THREE.MeshBasicMaterial({
+          color: 0x00ff00,
+        });
+        // Add the "bouton" object to the scene
+        scene.add(iBouton);
+
+        const aiguilleHeures = daeModel.getObjectByName("aiguille_heures");
+        aiguilleHeures.material = new THREE.MeshBasicMaterial({
+          color: 0xf0f0f0,
+        });
+        // Add the "aiguille_heures" object to the scene
+        scene.add(aiguilleHeures);
+
+        const aiguilleMinutes = daeModel.getObjectByName("aiguille_minutes");
+        aiguilleMinutes.material = new THREE.MeshBasicMaterial({
+          color: 0x0f0f0f,
+        });
+        // Add the "aiguille_minutes" object to the scene
+        scene.add(aiguilleMinutes);
+
+        const iPierre = daeModel.getObjectByName("pierre");
+        iPierre.material = new THREE.MeshBasicMaterial({
+          color: 0x0000ff,
+        });
+        // Add the "pierre" object to the scene
+        scene.add(iPierre);
+
+        const iBracelet = daeModel.getObjectByName("bracelet");
+        iBracelet.material = new THREE.MeshBasicMaterial({
+          color: 0x00f0ff,
+        });
+        // Add the "bracelet" object to the scene
+        scene.add(iBracelet);
+
+        const iFermoir = daeModel.getObjectByName("fermoir");
+        iFermoir.material = new THREE.MeshBasicMaterial({
+          color: 0xf000ff,
+        });
+        // Add the "fermoir" object to the scene
+        scene.add(iFermoir);
+      });
+
+      // Animation
+      const animate = () => {
+        requestAnimationFrame(animate);
+
+        // Render the scene
+        renderer.render(scene, camera);
+      };
+
+      // Start the animation
+      animate();
     },
   },
 };
 </script>
 
 <style scoped>
-/* Ajoutez des styles si nécessaire */
+/* #scene {
+  width: 1000px;
+  height: 600px;
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import ThreeSceneVue from "@/components/ThreeScene.vue";
   <div>
     <div></div>
     <div class="my-2 mx-2">
-      <!-- <ThreeSceneVue></ThreeSceneVue> -->
+      <ThreeSceneVue></ThreeSceneVue>
     </div>
   </div>
 </template>
